a0cc1fcedd9c609953e005584a731cbcaaa46a12
Mihai <mijaydobra04@gmail.com>
2025-09-11 21:31:23 +0300

    Small corrections

--- src/main.cpp
+++ src/main.cpp
@@ -6,52 +6,61 @@
 #include <ESP32Servo.h>
 #include <Wire.h>
 #include <DHT.h>
-#include <DHT_U.h>
 #include <ArduinoJson.h>
+#include "credentials.h"
 
-// --- WIFI credentials ---
-const char* ssid = "Internet Digi Daliei";
-const char* password = "A;`AQpf/M7&k";
 
 // --- MQTT Broker details ---
 const char* mqtt_server = "abf09ccff5484da78e04fb5444f1faf9.s1.eu.hivemq.cloud";
 const int mqtt_port = 8883;   // Secure port for MQTT over TLS
-const char* mqtt_user = "Mijay";
-const char* mqtt_password = "sHf728/ns@a8";
-const char* mqtt_client_id = "ESP32_Greenhouse_Client";
+const char* mqtt_client_id = "ESP32_Greenhouse_Client_RTOS";
 
 // --- MQTT Topics ---
-const char* topic_telemetry = "greenhouse/telemetry";
-const char* topic_commands = "greenhouse/commands";
-
-// TODO: los topicos de abajo quedaran obsoletos.
-const char* topic_soil_moisture = "greenhouse/sensor/soil_moisture";
-const char* topic_light_intensity = "greenhouse/sensor/env/light_intensity";
-const char* topic_temperature = "greenhouse/sensor/env/temperature";
-const char* topic_humidity = "greenhouse/sensor/env/humidity";
-const char* topic_pump_control = "greenhouse/actuator/pump_control";
-const char* topic_env_sensor = "greenhouse/sensor/env";
+const char* topic_telemetry = "greenhouse/1/telemetry";
+const char* topic_commands = "greenhouse/1/commands";
 
 // --- Define GPIO pins ---
-const uint8_t pin_pump = D4;  
+const uint8_t pin_pump = D2;  
 const uint8_t pin_servo = D6;
-const uint8_t pin_fan_intake = D3;
-const uint8_t pin_fan_exhaust = D2;
-const uint8_t pin_dht22 = 8;
-const uint8_t soil_sensors_pin[] = {A0,A1,A2,A3}; // Array of soil moisture sensor pins
+const uint8_t pin_fan_intake = D4;
+const uint8_t pin_fan_exhaust = D3;
+const uint8_t pin_dht22 = D5;
+const uint8_t soil_sensors_pin[] = {A0,A1,A2,A3};
 const uint8_t pin_led_r = D8;
-const uint8_t pin_led_g = D7; 
-const uint8_t pin_led_b = D9;
+const uint8_t pin_led_g = D9; 
+const uint8_t pin_led_b = D7;
 
-// --- Crop profiles (example for lettuce) ---
-struct PlantProfile {
-  float temp_max = 24.0;
-  float temp_min = 18.0;
-  float humidity_max = 70.0;
-  int soil_moisture_target = 60;
-  float light_lux_target = 15000.0;
+// --- Window servo positions ---
+const int window_close = 104;
+const int window_open = 30;  
+
+// --- Operation modes ---
+enum OperatingMode {AUTO, MANUAL};
+OperatingMode currentMode = MANUAL;  // Default mode 
+
+// --- Detailed crop profiles ---
+struct LightRecipe{
+  int r,g,b;
 };
-PlantProfile currentProfile;
+struct PlantProfile {
+  const char* name;
+  float temp_max;
+  float temp_min;
+  int humidity_max;
+  int soil_moisture_target;
+  float light_lux_target;
+  int hours_of_light;
+  LightRecipe germination_light;
+  LightRecipe growth_light;
+  LightRecipe flowering_light;
+};
+
+// --- Data base of plant profiles ---
+PlantProfile lettuceProfile = {"lettuce", 22.0, 15.0, 80, 65, 12000, 14, {100,100,255}, {255,100,200}, {255,150,150}};
+PlantProfile tomatoProfile = {"tomato", 26.0, 18.0, 70, 60, 20000, 16, {120,120,255}, {255,80,180}, {255,200,100}};
+
+PlantProfile* currentProfile = &lettuceProfile; // Puntero al perfil actual
+LightRecipe* currentLightRecipe = &lettuceProfile.growth_light; // Puntero al perfil de luz actual
 
 // --- Data structures for sensor data and actuator states ---
 struct SensorData {
@@ -67,15 +76,17 @@ struct ActuatorState {
   int pump;         // 0 or 1
   int fan_intake;
   int fan_exhaust;
-  int window_angle; // 0-180
-  struct { int r, g, b; } led_rgb;
+  int window;
+  LightRecipe led_rgb;
 };
+
+// --- Current manual state ---
+ActuatorState currentManualState = {0, 0, 0, window_close, {0, 0, 0}};
 
 // --- Global objects ---
 Servo servo;
 BH1750 lightSensor; // Sensor connected over I2C
-DHT dht(pin_dht22, DHT11);
-
+DHT dht(pin_dht22, DHT22);
 WiFiClientSecure espClient;   
 PubSubClient client(espClient);
 
@@ -85,16 +96,15 @@ QueueHandle_t actuatorCommandQueue;
 QueueHandle_t telemetryQueue;
 SemaphoreHandle_t i2cMutex;
 
-// --- Declare task functions ---
-/// @param pvParameters 
+// --- Declare task and functions ---
 void TaskSensors(void *pvParameters);
 void TaskControl(void *pvParameters);
 void TaskActuators(void *pvParameters); 
 void TaskComm(void *pvParameters);
+void callback(char* topic, byte* message, unsigned int lenght);
 
 // lux to ppfd conversion
 float luxToPPFD(float lux) {
-  // Approximate conversion factor
   const float conversionFactor = 0.0185; // This factor can vary based on light source
   return lux * conversionFactor;
 }
@@ -116,7 +126,7 @@ bool initDHT() {
 }
 
 unsigned long lastMsg = 0;
-const int msg_interval = 10000; // Interval between sending messages
+const int msg_interval = 10000;
 
 // --- Function to connect to WiFi ---
 void setup_wifi() {
@@ -138,7 +148,7 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // --- Initialize NTP for time synchronization ---
+  // Initialize NTP for time synchronization 
   configTime(0, 0, "pool.ntp.org", "time.nist.gov");
   Serial.print("Waiting for NTP time sync: ");
   time_t now = time(nullptr);
@@ -152,7 +162,6 @@ void setup_wifi() {
   gmtime_r(&now, &timeinfo);
   Serial.print("Current time: ");
   Serial.println(asctime(&timeinfo));
-
 }
 
 // --- MQTT callback function ---
@@ -164,23 +173,55 @@ void callback(char* topic, byte* message, unsigned int lenght){
   String messageTemp;
 
   for (int i = 0; i < lenght; i++) {
-    Serial.print((char)message[i]);   // send the caracters we receive in the serial monitor
+    Serial.print((char)message[i]);  
     messageTemp += (char)message[i];
   }
   Serial.println();
 
-  // Y aqui en funcion de que mensaje recibimos en el topic, haremos una cosa u otra
-  // POr ejemplo para el topic de light_intensity, (pero esto no es practico, solo ejemplo, habra que cambiarlo)
-    if (String(topic) == topic_light_intensity){
-    Serial.print("Changing light intensity to ");
-    if(messageTemp == "on"){
-      Serial.println("ON");
-      // Activar el sensor de luz
-    } else if(messageTemp == "off"){
-      Serial.println("OFF");
-      // Desactivar el sensor de luz
-    }
+  // Convert the message to a JSON document
+  JsonDocument doc;
+  DeserializationError error = deserializeJson(doc, message, lenght);
+  if (error) {
+    Serial.print("Failed to parse JSON: ");
+    Serial.println(error.c_str());
+    return;
   }
+
+  // Parse JSON content
+  const char* command = doc["command"];
+
+  if (strcmp(command, "set_profile") == 0){
+    const char* profileName = doc["profile_name"];
+    if (strcmp(profileName, "lettuce") == 0) currentProfile = &lettuceProfile;
+    else if (strcmp(profileName, "tomato") == 0) currentProfile = &tomatoProfile;
+    currentMode = AUTO; 
+    Serial.printf("Profile set to: '%s'. Mode: AUTO\n", profileName);
+
+  } else if (strcmp(command, "set_light_stage") == 0) {
+    const char* stage = doc["stage"];
+    if (strcmp(stage, "germination") == 0) currentLightRecipe = &currentProfile->germination_light;
+    else if (strcmp(stage, "growth") == 0) currentLightRecipe = &currentProfile->growth_light;
+    else if (strcmp(stage, "flowering") == 0) currentLightRecipe = &currentProfile->flowering_light;
+    Serial.printf("Light stage set to: '%s'.\n", stage);
+  }
+  else if (strcmp(command, "manual_override") == 0){
+    currentMode = MANUAL;  // whatever comand manual we receive activate manual mode
+
+    // The command can afect only one actuator, so we read the JSON
+    if (!doc["pump"].isNull()) currentManualState.pump = doc["pump"];
+    if (!doc["fan_intake"].isNull()) currentManualState.fan_intake = doc["fan_intake"];
+    if (!doc["fan_exhaust"].isNull()) currentManualState.fan_exhaust = doc["fan_exhaust"];
+    if (!doc["window"].isNull()) currentManualState.window = doc["window"];
+    if (!doc["led_rgb"].isNull()) {
+      currentManualState.led_rgb.r = doc["led_rgb"]["r"];
+      currentManualState.led_rgb.g = doc["led_rgb"]["g"];
+      currentManualState.led_rgb.b = doc["led_rgb"]["b"];
+    }
+    
+    xQueueSend(actuatorCommandQueue, &currentManualState, 0);
+    Serial.printf("Manual command applied.");
+  }
+
 }
 
 // --- Function to reconnect to MQTT broker ---
@@ -189,9 +230,7 @@ void reconect(){
     Serial.print("Attempting MQTT conncetion...");
     if (client.connect(mqtt_client_id, mqtt_user, mqtt_password)){
       Serial.println("connected");
-      // Subscribe
-      // TODO: Habra que cambiar los topicos a los que se suscribe
-      client.subscribe(topic_light_intensity); 
+      client.subscribe(topic_commands); 
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -202,16 +241,14 @@ void reconect(){
   }
 }
 
-
 // --- Task to read sensors ---
 void TaskSensors(void *pvParameters) {
-  (void) pvParameters;
   SensorData data;
   TickType_t xLastWakeTime = xTaskGetTickCount();
   const TickType_t xFrequency = pdMS_TO_TICKS(5000); // Read sensors every 5 seconds
 
-  const int dry = 2650;
-  const int wet = 1080;
+  const int dry = 930;
+  const int wet = 550;
 
   for (;;){
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
@@ -221,24 +258,25 @@ void TaskSensors(void *pvParameters) {
 
     // This semaphore is used to avoid conflicts on the I2C bus
     if (xSemaphoreTake(i2cMutex, portMAX_DELAY) == pdTRUE) {
-      data.lux = lightSensor.readLightLevel();
-      data.ppfd = luxToPPFD(data.lux);
+      data.lux = round(lightSensor.readLightLevel() *100.0) / 100.0;
+      data.ppfd = round(luxToPPFD(data.lux) * 100.0) / 100.0;
       xSemaphoreGive(i2cMutex);
     }
 
     float soil_sum = 0;
 
     for (int i = 0; i < 4; i++){
-      data.soil_moisture[i] = map(analogRead(soil_sensors_pin[i]), wet, dry, 100, 0);
+      int raw_value = analogRead(soil_sensors_pin[i]);
+      int mapped_value = map(raw_value, wet, dry, 100, 0);
+      data.soil_moisture[i] = constrain(mapped_value, 0, 100);
       soil_sum += data.soil_moisture[i];
     }
     data.soil_avg = soil_sum / 4.0;
 
-    // Send data to queue
+    // Send data to queues
     xQueueSend(sensorDataQueue, &data, portMAX_DELAY);
     xQueueSend(telemetryQueue, &data, portMAX_DELAY);
     
-    //Debug
     Serial.println("[TaskSensors] Readings sent to queue.");
 
   }
@@ -252,12 +290,16 @@ void TaskControl(void *pvParameters){
 
   for(;;){
     if (xQueueReceive(sensorDataQueue, &receivedData, portMAX_DELAY) == pdPASS) {
-      // Control logic based on the plant profile 
       
-      // TODO: Se implementara una logica de control como PID o por reinforcement learning con TinyML
+      // If we are in manual mode, skip control logic
+      if (currentMode == MANUAL) {
+        continue;
+      }
 
-      // Temperature control
-      if (receivedData.temperature > currentProfile.temp_max){
+      // TODO: See if a control logic like PID or reinforcement learning with TinyML will be implemented.
+
+      // Control logic based on currentProfile
+      if (receivedData.temperature > currentProfile->temp_max){
         newState.fan_exhaust = HIGH;
         newState.fan_intake = HIGH;
       } else {
@@ -266,16 +308,16 @@ void TaskControl(void *pvParameters){
       }
       
       // Pump control
-      newState.pump = (receivedData.soil_avg < currentProfile.soil_moisture_target) ? HIGH : LOW;
+      newState.pump = (receivedData.soil_avg < currentProfile->soil_moisture_target) ? HIGH : LOW;
 
-      // Servo control for window
-      newState.window_angle = (receivedData.temperature > currentProfile.temp_max - 2) ? 90 : 0; // Open window if temp is 2 degrees above max
+      // Servo control for window 
+      newState.window = (receivedData.temperature > currentProfile->temp_max - 2) ? 1 : 0; 
 
+      
       // LED Strip control
-      // TODO: Tambien ver en que datos basarnos lux o ppfd
-      if (receivedData.lux < currentProfile.light_lux_target) {
-        // TODO: Habra que ajustar el color y la intensidad en funcion de planta mediante su profil y tambien mediante lo que indique el usuario en la app si esta en germinacion, crecimiento o floracion
-        newState.led_rgb = {255, 255, 255}; // White light if below target
+      // TODO: Add logic based on time of day using NTP
+      if (receivedData.lux < currentProfile->light_lux_target) {
+        newState.led_rgb = *currentLightRecipe;
       } else {
         newState.led_rgb = {0, 0, 0}; // Turn off if above target
       }
@@ -287,8 +329,9 @@ void TaskControl(void *pvParameters){
 
 // --- Task to control actuators ---
 void TaskActuators(void *pvParameters){
-  (void) pvParameters;
+  const int tolerance = 4;
   ActuatorState state;
+
   for (;;){
     if(xQueueReceive(actuatorCommandQueue, &state, portMAX_DELAY) == pdPASS){
       
@@ -298,9 +341,23 @@ void TaskActuators(void *pvParameters){
       digitalWrite(pin_pump, state.pump);
       digitalWrite(pin_fan_intake, state.fan_intake);
       digitalWrite(pin_fan_exhaust, state.fan_exhaust);
-      servo.write(state.window_angle);
 
-      // TODO: ver si el analogWrite funciona bien
+      int currentPos = servo.read();
+      //Serial.printf("[Servo] Command: %d, Current: %d, Open: %d, Close: %d\n", state.window, currentPos, window_open, window_close);
+
+      if(state.window == 1 && abs(currentPos - window_close) <= tolerance){
+        for (int pos = window_close; pos >= window_open; pos -= 1) {
+          servo.write(pos);
+          vTaskDelay(pdMS_TO_TICKS(30));
+        }
+      }
+      if(state.window == 0 && abs(currentPos - window_open) <= tolerance){
+        for (int pos = window_open; pos <= window_close; pos += 1) {
+          servo.write(pos);
+          vTaskDelay(pdMS_TO_TICKS(30));
+        }
+      }
+
       analogWrite(pin_led_r, state.led_rgb.r);
       analogWrite(pin_led_g, state.led_rgb.g);
       analogWrite(pin_led_b, state.led_rgb.b);
@@ -311,28 +368,29 @@ void TaskActuators(void *pvParameters){
 
 // --- Task to handle communications ---
 void TaskComm(void *pvParameters){
-  (void) pvParameters;
-  SensorData telemetryData;
 
+  SensorData telemetryData;
   // Conect to WiFi
   setup_wifi();
 
-  // conect to MQTT broker
+  // Configure MQTT client
   espClient.setInsecure(); 
   client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
+  client.setCallback(callback); 
 
   for(;;){
+    // Ensure MQTT connection
     if (!client.connected()) {
       reconect();
     }
     client.loop();
-
    
-    // Publis telemetry if data is available
+    // Publish telemetry if data is available
     if (xQueueReceive(telemetryQueue, &telemetryData, 0) == pdPASS) {
       // Add data to JSON document 
       JsonDocument doc;
+      doc["profile"] = currentProfile->name;
+      doc["mode"] = (currentMode == AUTO) ? "AUTO" : "MANUAL";
       doc["temperature"] = telemetryData.temperature;
       doc["humidity"] = telemetryData.humidity;
       doc["lux"] = telemetryData.lux;
@@ -347,6 +405,7 @@ void TaskComm(void *pvParameters){
       char buffer[256];
       serializeJson(doc, buffer);
       client.publish(topic_telemetry, buffer);
+
       Serial.printf("Published to %s: %s\n", topic_telemetry, buffer);
     }
     vTaskDelay(pdMS_TO_TICKS(100)); // Small delay to avoid busy loop
@@ -359,11 +418,9 @@ void setup() {
 
   // Start Serial connection 
   Serial.begin(115200);
-  Wire.begin();       // Initialize I2C
-  while(!Serial);
+  Wire.begin();       
 
-  // Initialize GPIO pins 
-
+  // Pin modes
   pinMode(pin_pump, OUTPUT);
   pinMode(pin_fan_intake, OUTPUT);
   pinMode(pin_fan_exhaust, OUTPUT);
@@ -383,19 +440,11 @@ void setup() {
     Serial.println("Could not find a valid DHT22 sensor, check wiring!");
     while(1);
   } 
+  servo.attach(pin_servo);
+  servo.write(window_close);
 
-  //if (!servo.attached()) {
-  //}
 
   Serial.println("Sensor initialization complete.");
-
-  // --- Connect to WiFi ---
-  //setup_wifi();
-
-  // --- Setup MQTT server details ---
-  //espClient.setInsecure(); 
-  //client.setServer(mqtt_server, mqtt_port);
-  //client.setCallback(callback);
 
   // Create queues and mutex
   sensorDataQueue = xQueueCreate(5, sizeof(SensorData));
@@ -407,13 +456,11 @@ void setup() {
   xTaskCreatePinnedToCore(TaskSensors, "SensorTask", 4096, NULL, 3, NULL, 0);
   xTaskCreatePinnedToCore(TaskControl, "ControlTask", 4096, NULL, 2, NULL, 0);
   xTaskCreatePinnedToCore(TaskActuators, "ActuatorTask", 2048, NULL, 2, NULL, 0);
-  xTaskCreatePinnedToCore(TaskComm, "CommTask", 4096, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(TaskComm, "CommTask", 5120, NULL, 1, NULL, 1);
 
-  
   Serial.println("FreeRTOS Setup complete.");
-
 }
+
 // --- Main loop function ---
 void loop() {
-  vTaskDelay(pdMS_TO_TICKS(1000)); // Keep the loop alive with a delay
-}
+  vTaskDelay(pdMS_TO_TICKS(1000)); 
